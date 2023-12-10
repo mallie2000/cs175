47bb433a84a65aaf77c34f4a47eb426d1ef141a5
mallie2000 <muzammilallie230@gmail.com>
2023-12-09 17:29:55 -0800

    Inital Repo

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home' });
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
